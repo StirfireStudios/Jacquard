bd96e0ea94d82109c8a8b0bed7ad8d9c9917b8f8
James Stewart <james.stewart@augmentedfifth.com>
2018-02-19 13:55:31 +0800

    Added Bash script to install Node dependencies

---
+++ scripts/node/node_install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# The main function of the script.
+main() {
+  # Install create-react-app
+  yarn add create-react-app --dev
+}
+
+# Call the main function with the arguments passed to the script.
+main "$@"
