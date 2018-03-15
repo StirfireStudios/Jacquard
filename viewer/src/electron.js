ff6830d4f75ef900712c879a576fe0b61b6a7c40
Garry Stewart <garry.stewart@augmentedfifth.com>
2018-03-15 23:30:34 +0800

    Moved the Viewer code and wrapped it in Electorn.

---
+++ viewer/src/electron.js
@@ -0,0 +1,31 @@
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+
+const path = require('path');
+const url = require('url');
+let mainWindow;
+
+function createWindow() {
+	mainWindow = new BrowserWindow({ width: 900, height: 680 });
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true,
+	}));
+	mainWindow.on('closed', () => { mainWindow = null; });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
