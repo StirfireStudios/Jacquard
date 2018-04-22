3dbc29ace647d300c1b6ed90101097bda8627d90
James Stewart <james.stewart@augmentedfifth.com>
2018-04-22 22:12:39 +0800

    Changed parameter order to be more consistent

--- frontend/public/electron.js
+++ frontend/public/electron.js
@@ -20,7 +20,7 @@ function createWindow() {
 	mainWindow.on('closed', () => { mainWindow = null; });
 }
 
-const currentProjectSave = (event, currentProjectJSON, currentProjectFilePath) => {
+const currentProjectSave = (event, currentProjectFilePath, currentProjectJSON) => {
 	// Write the project JSON to disk
 	fs.writeFile(currentProjectFilePath, currentProjectJSON, (err) => {
 		// Notify that the project file path has changed
@@ -42,7 +42,7 @@ const currentProjectSave = (event, currentProjectJSON, currentProjectFilePath) =
 	});
 };
 
-const currentProjectSaveAs = (event, currentProjectJSON, currentProjectFilePath) => {
+const currentProjectSaveAs = (event, currentProjectFilePath, currentProjectJSON) => {
 	// Get the current directory from the project file path (if we have one)
 	const currentDirectoryPath = (currentProjectFilePath)
 		? path.dirname(currentProjectFilePath)
@@ -72,7 +72,7 @@ const currentProjectSaveAs = (event, currentProjectJSON, currentProjectFilePath)
 	);
 };
 
-const currentProjectExportAsYarn = (event, currentProjectYarn, currentProjectFilePath) => {
+const currentProjectExportAsYarn = (event, currentProjectFilePath, currentProjectYarn) => {
 	// Get the current directory from the project file path (if we have one)
 	const currentDirectoryPath = (currentProjectFilePath)
 		? path.dirname(currentProjectFilePath)
@@ -178,24 +178,24 @@ app.on('activate', () => {
 
 ipcMain.on('projectSave', (event, arg) => {
 	// Get the project info from the argument
-	const { currentProjectJSON, currentProjectFilePath } = arg;
+	const { currentProjectFilePath, currentProjectJSON } = arg;
 
 	// Do we not have a project file path?
 	if (!currentProjectFilePath) {
 		// Ask the user to save the project under a different file path
-		currentProjectSaveAs(event, currentProjectJSON, currentProjectFilePath);
+		currentProjectSaveAs(event, currentProjectFilePath, currentProjectJSON);
 	}
 
 	// Save the current project JSON
-	currentProjectSave(event, currentProjectJSON, currentProjectFilePath);
+	currentProjectSave(event, currentProjectFilePath, currentProjectJSON);
 });
 
 ipcMain.on('projectSaveAs', (event, arg) => {
 	// Get the project info from the argument
-	const { currentProjectJSON, currentProjectFilePath } = arg;
+	const { currentProjectFilePath, currentProjectJSON } = arg;
 
 	// Ask the user to save the project under a different file path
-	currentProjectSaveAs(event, currentProjectJSON, currentProjectFilePath);
+	currentProjectSaveAs(event, currentProjectFilePath, currentProjectJSON);
 });
 
 ipcMain.on('projectOpen', (event, currentProjectFilePath) => {
