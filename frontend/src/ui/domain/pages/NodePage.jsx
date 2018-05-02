eb0cb37b2408bb372df03b53d0c52310f6f6ce11
James Stewart <james.stewart@augmentedfifth.com>
2018-05-03 00:06:09 +0800

    Added validation icon indicator next to each node in the list, and general validation errors/warnings at the bottom of the page

--- frontend/src/ui/domain/pages/NodePage.jsx
+++ frontend/src/ui/domain/pages/NodePage.jsx
@@ -3,7 +3,16 @@ import PropTypes from 'prop-types';
 
 import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
+import Tooltip from 'material-ui/Tooltip';
+import Icon from 'material-ui/Icon';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import ErrorIcon from '@material-ui/icons/Error';
+import WarningIcon from '@material-ui/icons/Warning';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+
 import red from 'material-ui/colors/red';
+import orange from 'material-ui/colors/orange';
+import green from 'material-ui/colors/green';
 
 import uuidv4 from 'uuid/v4';
 
@@ -15,6 +24,21 @@ import NodeEditor from '../modals/NodeEditor';
 import yarnService from '../../../services/yarnService';
 
 const styles = theme => ({
+	errorIcon: {
+		width: 48,
+		height: 48,
+		color: red[500],
+	},
+	warningIcon: {
+		width: 48,
+		height: 48,
+		color: orange[500],
+	},
+	okIcon: {
+		width: 48,
+		height: 48,
+		color: green[500],
+	},
 	nodeExistingLinkButton: {
 		marginRight: theme.spacing.unit * 2,
 	},
@@ -108,11 +132,136 @@ const getOutgoingLinkButtons = (projectNodeLinks, classes, item, field, onAddIte
 	});
 };
 
+const getValidationResultIcon = (projectValidationResults, classes, item) => {
+	// If we don't have project validation results, return an empty result
+	if (!projectValidationResults) {
+		return null;
+	}
+
+	// Get the node validation results
+	const nodeValidationResults = projectValidationResults.nodes[item.title];
+
+	// If we don't have any node validation results, return an empty result
+	if (!nodeValidationResults) {
+		return null;
+	}
+
+	// The validation result icon
+	let validationResultIcon = null;
+
+	// Build an icon if the node has errors
+	if (nodeValidationResults.errors.length > 0) {
+		// Build the error icon tooltip (use only the first one)
+		const errorIconTooltip = nodeValidationResults.errors[0].message;
+
+		// Build the error icon
+		const errorIcon = (
+			<Tooltip key={uuidv4()} title={errorIconTooltip}>
+				<ErrorIcon className={classes.errorIcon} />
+			</Tooltip>
+		);
+
+		// Set it as the validation result icon
+		validationResultIcon = errorIcon;
+	} else if (nodeValidationResults.warnings.length > 0) {
+		// Build the warning icon tooltip (use only the first one)
+		const warningIconTooltip = nodeValidationResults.warnings[0].message;
+
+		// Build the warning icon
+		const warningIcon = (
+			<Tooltip key={uuidv4()} title={warningIconTooltip}>
+				<WarningIcon className={classes.warningIcon} />
+			</Tooltip>
+		);
+
+		// Set it as the validation result icon
+		validationResultIcon = warningIcon;
+	} else {
+		// Set the validation result icon to the OK icon
+		validationResultIcon = <CheckCircleIcon className={classes.okIcon} />;
+	}
+
+	return validationResultIcon;
+};
+
+const renderGeneralValidationErrors = projectValidationResults =>
+	// Build the validation errors
+	projectValidationResults.generalErrors.map((validationError) => {
+		// Build the location
+		const location = yarnService.buildLocationString(validationError.location);
+
+		return (
+			<ListItem key={uuidv4()}>
+				<Icon color="error">error</Icon>
+				<ListItemText primary={validationError.message} secondary={location} />
+			</ListItem>
+		);
+	});
+
+const renderGeneralValidationWarnings = projectValidationResults =>
+	// Build the validation warnings
+	projectValidationResults.generalWarnings.map((validationWarning) => {
+		// Build the location
+		const location = yarnService.buildLocationString(validationWarning.location);
+
+		return (
+			<ListItem key={uuidv4()}>
+				<Icon>warning</Icon>
+				<ListItemText
+					primary={validationWarning.message}
+					secondary={location}
+				/>
+			</ListItem>
+		);
+	});
+
+const renderGeneralValidationResults = (projectValidationResults) => {
+	// Render the validation errors and warnings
+	const validationErrors = renderGeneralValidationErrors(projectValidationResults);
+	const validationWarnings = renderGeneralValidationWarnings(projectValidationResults);
+
+	// The validation results
+	const validationResults = [];
+
+	// If we have validation errors, add them to the results
+	if ((validationErrors) && (validationErrors.length > 0)) {
+		validationResults.push((
+			<div key={uuidv4()}>
+				<h2>General Errors</h2>
+				<List>
+					{validationErrors}
+				</List>
+			</div>
+		));
+	}
+
+	// If we have validation warings, add them to the results
+	if ((validationWarnings) && (validationWarnings.length > 0)) {
+		validationResults.push((
+			<div key={uuidv4()}>
+				<h2>General Warnings</h2>
+				<List>
+					{validationWarnings}
+				</List>
+			</div>
+		));
+	}
+
+	return validationResults;
+};
+
 class NodeListPage extends React.Component {
 	render() {
+		// Validate the project nodes
+		const projectValidationResults = yarnService
+			.validateProjectNodes(this.props.currentProjectFilePath, this.props.currentProject.nodes);
+
 		// Get the project node links
 		const projectNodeLinks = yarnService
 			.getProjectNodeLinks(this.props.currentProjectFilePath, this.props.currentProject.nodes);
+
+		// Render the general validation results
+		const generalValidationResults = renderGeneralValidationResults(projectValidationResults);
 
 		// The list fields
 		const fields = [
@@ -157,6 +306,19 @@ class NodeListPage extends React.Component {
 						onEditItemClick,
 					),
 			},
+			{
+				name: 'validationResults',
+				displayName: 'Validation',
+				getContentCallback: (item, field, onAddItemClick, onEditItemClick) =>
+					getValidationResultIcon(
+						projectValidationResults,
+						this.props.classes,
+						item,
+						field,
+						onAddItemClick,
+						onEditItemClick,
+					),
+			},
 		];
 
 		const addEditForm = props => (
@@ -166,55 +328,58 @@ class NodeListPage extends React.Component {
 			/>);
 
 		return (
-			<ListPage
-				onCurrentProjectChanged={this.props.onCurrentProjectChanged}
-				fields={fields}
-				keyName="title"
-				currentProject={this.props.currentProject}
-				currentProjectFilePath={this.props.currentProjectFilePath}
-				currentProjectPropName="nodes"
-				editFormTitle="Edit Node"
-				addFormTitle="Add Node"
-				addEditForm={addEditForm}
-				formSchema={[
-					{
-						fieldName: 'title',
-						label: 'Title',
-						readOnly: false,
-						multiline: false,
-					},
-					{
-						fieldName: 'tags',
-						label: 'Tags',
-						readOnly: false,
-						multiline: false,
-					},
-					{
-						fieldName: 'section',
-						label: 'Section',
-						readOnly: false,
-						multiline: false,
-					},
-					{
-						fieldName: 'colorId',
-						label: 'Color ID',
-						readOnly: false,
-						multiline: false,
-					},
-					{
-						fieldName: 'position',
-						label: 'Position',
-						readOnly: false,
-						multiline: false,
-					},
-					{
-						fieldName: 'body',
-						label: 'Body',
-						readOnly: false,
-						multiline: false,
-					},
-				]}
-			/>
+			<div>
+				<ListPage
+					onCurrentProjectChanged={this.props.onCurrentProjectChanged}
+					fields={fields}
+					keyName="title"
+					currentProject={this.props.currentProject}
+					currentProjectFilePath={this.props.currentProjectFilePath}
+					currentProjectPropName="nodes"
+					editFormTitle="Edit Node"
+					addFormTitle="Add Node"
+					addEditForm={addEditForm}
+					formSchema={[
+						{
+							fieldName: 'title',
+							label: 'Title',
+							readOnly: false,
+							multiline: false,
+						},
+						{
+							fieldName: 'tags',
+							label: 'Tags',
+							readOnly: false,
+							multiline: false,
+						},
+						{
+							fieldName: 'section',
+							label: 'Section',
+							readOnly: false,
+							multiline: false,
+						},
+						{
+							fieldName: 'colorId',
+							label: 'Color ID',
+							readOnly: false,
+							multiline: false,
+						},
+						{
+							fieldName: 'position',
+							label: 'Position',
+							readOnly: false,
+							multiline: false,
+						},
+						{
+							fieldName: 'body',
+							label: 'Body',
+							readOnly: false,
+							multiline: false,
+						},
+					]}
+				/>
+				{generalValidationResults}
+			</div>
 		);
 	}
 }
