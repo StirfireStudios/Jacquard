e9470e9486a3b1431042f2ccf12d4c349b3a3957
James Stewart <james.stewart@augmentedfifth.com>
2018-05-06 19:06:56 +0800

    Modal dialogs button labels can now be specified

--- frontend/src/ui/general/components/ModalDialog.jsx
+++ frontend/src/ui/general/components/ModalDialog.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Dialog, {
 	DialogActions,
 	DialogContent,
@@ -6,33 +8,45 @@ import Dialog, {
 } from 'material-ui/Dialog';
 import Button from 'material-ui/Button';
 
-class ModalDialog extends React.Component {
-	render() {
-		return (
-			<div>
-				<Dialog
-					open={this.props.open}
-					onClose={this.props.onCancel}
-					aria-labelledby="form-dialog-title"
-				>
-					<DialogTitle id="form-dialog-title">
-						{ this.props.title }
-					</DialogTitle>
-					<DialogContent>
-						{ this.props.children }
-					</DialogContent>
-					<DialogActions>
-						<Button onClick={this.props.onCancel} variant="raised" color="secondary">
-							Cancel
-						</Button>
-						<Button onClick={this.props.onSave} variant="raised" color="primary">
-							Save
-						</Button>
-					</DialogActions>
-				</Dialog>
-			</div>
-		);
-	}
-}
+const ModalDialog = props => (
+	<div>
+		<Dialog
+			open={props.open}
+			onClose={props.onCancel}
+			aria-labelledby="form-dialog-title"
+		>
+			<DialogTitle id="form-dialog-title">
+				{ props.title }
+			</DialogTitle>
+			<DialogContent>
+				{ props.children }
+			</DialogContent>
+			<DialogActions>
+				<Button onClick={props.onCancel} variant="raised" color="secondary">
+					{props.cancelButtonLabel}
+				</Button>
+				<Button onClick={props.onSave} variant="raised" color="primary">
+					{props.okButtonLabel}
+				</Button>
+			</DialogActions>
+		</Dialog>
+	</div>
+);
+
+ModalDialog.propTypes = {
+	onSave: PropTypes.func.isRequired,
+	onCancel: PropTypes.func.isRequired,
+
+	title: PropTypes.string.isRequired,
+	open: PropTypes.bool.isRequired,
+
+	okButtonLabel: PropTypes.string,
+	cancelButtonLabel: PropTypes.string,
+};
+
+ModalDialog.defaultProps = {
+	okButtonLabel: 'OK',
+	cancelButtonLabel: 'Cancel',
+};
 
 export default ModalDialog;
