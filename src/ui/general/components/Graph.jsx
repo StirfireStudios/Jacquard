644d7e81011a2fd2d070a324c0c56b2e94448450
James Stewart <james.stewart@augmentedfifth.com>
2018-05-11 05:08:16 +0800

    Fixed linting errors

--- src/ui/general/components/Graph.jsx
+++ src/ui/general/components/Graph.jsx
@@ -5,114 +5,95 @@ import GraphView from 'react-digraph';
 
 // The styles of the component
 const styles = {
-    // The graph style
-    graph: {
-      height: '100%',
-      width: '100%'
-    }
+	// The graph style
+	graph: {
+		height: '100%',
+		width: '100%',
+	},
 };
 
 class Graph extends React.Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        // Set up the state of the component
-        this.state = {
-            selected: {}
-        };
-    }
+		// Set up the state of the component
+		this.state = {
+			selected: {},
+		};
+	}
 
-    // Given a nodeKey, return the corresponding node
-    getViewNode = (nodeKeyValue) => {
-        // Find the node index
-        const nodeIndex = this.findNodeIndex(nodeKeyValue);
+	/* Define custom graph editing methods here */
+	onSelectNode = () => {}
+	onCreateNode = () => {}
+	onUpdateNode = () => {}
+	onDeleteNode = () => {}
+	onSelectEdge = () => {}
+	onCreateEdge = () => {}
+	onSwapEdge = () => {}
+	onDeleteEdge = () => {}
 
-        // Get the node
-        const node = this.props.graph.nodes[nodeIndex];
+	// Given a nodeKey, return the corresponding node
+	getViewNode = (nodeKeyValue) => {
+		// Find the node index
+		const nodeIndex = this.findNodeIndex(nodeKeyValue);
 
-        console.log(`getViewNode() - ${nodeKeyValue}, ${nodeIndex}`);
-        console.log(node);
+		// Get the node
+		const node = this.props.graph.nodes[nodeIndex];
 
-        return node;
-    }
+		console.log(`getViewNode() - ${nodeKeyValue}, ${nodeIndex}`);
+		console.log(node);
 
-    findNodeIndex = (nodeKeyValue) =>
-        this.props.graph.nodes
-            .findIndex(node => (node[this.props.nodeKey] === nodeKeyValue));
+		return node;
+	}
 
+	findNodeIndex = nodeKeyValue =>
+		this.props.graph.nodes
+			.findIndex(node => (node[this.props.nodeKey] === nodeKeyValue));
 
-    /* Define custom graph editing methods here */
-    onSelectNode = () => {}
-    onCreateNode = () => {}
-    onUpdateNode = () => {}
-    onDeleteNode = () => {}
-    onSelectEdge = () => {}
-    onCreateEdge = () => {}
-    onSwapEdge = () => {}
-    onDeleteEdge = () => {}
-
-    render() {
-        // Get the nodes
-        const nodes = this.props.graph.nodes;
-
-        // Get the edges
-        const edges = this.props.graph.edges;
-
-        // Get the selected nodes
-        const selected = this.state.selected;
-
-        // Get the node types
-        const nodeTypes = this.props.graphConfig.nodeTypes;
-
-        // Get the node sub-types
-        const nodeSubtypes = this.props.graphConfig.nodeSubtypes;
-
-        // Get the edge types
-        const edgeTypes = this.props.graphConfig.edgeTypes;
-
-        return (
-            <div id='graph' style={styles.graph}>
-                <GraphView 
-                    ref='GraphView'
-                    nodeKey={this.props.nodeKey}
-                    emptyType={this.props.emptyType}
-                    nodes={nodes}
-                    edges={edges}
-                    selected={selected}
-                    nodeTypes={nodeTypes}
-                    nodeSubtypes={nodeSubtypes}
-                    edgeTypes={edgeTypes}
-                    readOnly
-                    graphControls
-                    maxTitleChars={100}
-                    getViewNode={this.getViewNode}
-                    onSelectNode={this.onSelectNode}
-                    onCreateNode={this.onCreateNode}
-                    onUpdateNode={this.onUpdateNode}
-                    onDeleteNode={this.onDeleteNode}
-                    onSelectEdge={this.onSelectEdge}
-                    onCreateEdge={this.onCreateEdge}
-                    onSwapEdge={this.onSwapEdge}
-                    onDeleteEdge={this.onDeleteEdge}
-                />
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div id="graph" style={styles.graph}>
+				<GraphView
+					ref={(c) => { this.GraphView = c; }}
+					nodeKey={this.props.nodeKey}
+					emptyType={this.props.emptyType}
+					nodes={this.props.graph.nodes}
+					edges={this.props.graph.edges}
+					selected={this.state.selected}
+					nodeTypes={this.props.graphConfig.nodeTypes}
+					nodeSubtypes={this.props.graphConfig.nodeSubtypes}
+					edgeTypes={this.props.graphConfig.edgeTypes}
+					readOnly
+					graphControls
+					maxTitleChars={100}
+					getViewNode={this.getViewNode}
+					onSelectNode={this.onSelectNode}
+					onCreateNode={this.onCreateNode}
+					onUpdateNode={this.onUpdateNode}
+					onDeleteNode={this.onDeleteNode}
+					onSelectEdge={this.onSelectEdge}
+					onCreateEdge={this.onCreateEdge}
+					onSwapEdge={this.onSwapEdge}
+					onDeleteEdge={this.onDeleteEdge}
+				/>
+			</div>
+		);
+	}
 }
 
 Graph.propTypes = {
-    graph: PropTypes.object,
-    graphConfig: PropTypes.object,
-    nodeKey: PropTypes.string.isRequired,
-    emptyType: PropTypes.string.isRequired,
+	graph: PropTypes.object,
+	graphConfig: PropTypes.object,
+	nodeKey: PropTypes.string.isRequired,
+	emptyType: PropTypes.string.isRequired,
 };
 
 Graph.defaultProps = {
 	graph: {
-        nodes: [],
-        edges: [],
-    },
-    graphConfig: {},
+		nodes: [],
+		edges: [],
+	},
+	graphConfig: {},
 };
 
 
